3f3e2796b07fabda1ed53f5a8888c40d98366afc
Mark Sonnabaum <marksonnabaum@gmail.com>
2011-11-15 14:25:42 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git@github.com:msonnabaum/rage-reducing-dotfiles.git
+git clone git@github.com:msonnabaum/rage-reducing-dotfiles.git
 ls rage-reducing-dotfiles/| while read i; do cp "${i}" ~/.${i}; done
