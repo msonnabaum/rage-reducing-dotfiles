feb47921fd231e7698f983aca02db134f728a2d4
Mark Sonnabaum <mark@sonnabaum.com>
2011-12-05 09:48:48 -0600

    Add update-alternatives editor command to get vim in crontab -e.

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 git clone git://github.com/msonnabaum/rage-reducing-dotfiles.git
 cd rage-reducing-dotfiles && ls | grep -v "install"| grep -v "README" | while read i; do cp "${i}" ~/.${i}; done
+
+# crontab -e and visudo dropping me in nano? WHOA, fuck that.
+update-alternatives --set editor /usr/bin/vim.basic
